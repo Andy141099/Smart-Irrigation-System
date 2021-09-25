92e3d52a7d1e8c1b3997d248dbe69c4cefe18a91
Andy141099 <71865320+Andy141099@users.noreply.github.com>
2021-09-25 14:38:35 +0530

    Update agriculture.ino

--- agriculture.ino
+++ agriculture.ino
@@ -79,7 +79,7 @@ int waterlevel(){
 }
 long microsecondsToCentimeters(long microseconds) {
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the object we
+  // The ping travels out and back, so to find the distance of the depth of water we
   // take half of the distance travelled.
   return microseconds / 29 / 2;
 }
